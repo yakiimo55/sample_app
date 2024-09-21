8fc5a2964dcc07c5092a8b3a77e522ff79a3075b
EC2 Default User <ec2-user@ip-172-31-15-229.ap-northeast-1.compute.internal>
2024-09-21 13:09:16 +0000

    CH.11 index.HTML.erb書き換えまで終了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,11 +5,15 @@ class ListsController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
+    if @list.save
     # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+      redirect_to list_path(@list.id)
+    else
+      @lists = List.all
+      render :index #<= new から indexに変更
+    end
   end
   
   def index
@@ -41,4 +45,5 @@ class ListsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
+
 end
